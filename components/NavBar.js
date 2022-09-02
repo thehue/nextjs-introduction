b1322740b93fc7a3753e4ea3e6fc6b3c5ef824c8
thehue <gouk32@naver.com>
2022-09-02 18:22:28 +0900

    feat: 1.3 Routing

---
+++ components/NavBar.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <Link href="/">
+        <a
+          className="hello"
+          style={{ color: router.pathname === "/" ? "red" : "black" }}
+        >
+          Home
+        </a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: router.pathname === "/about" ? "red" : "black" }}>
+          About
+        </a>
+      </Link>
+    </nav>
+  );
+}
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import NavBar from "../components/NavBar";
 
-export default function Home() {
+export default function About() {
   return (
     <div>
       <NavBar />
-      <h1>Hello</h1>
+      about us
     </div>
   );
 }
