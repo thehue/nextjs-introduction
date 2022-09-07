32b192f967b709adf2d3d93c8844576170fde608
thehue <gouk32@naver.com>
2022-09-07 11:02:55 +0900

    2.2 Redirects and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
